f41adf7f3f93737f7f0847b975989cb2be92ce3e
Kevin Lin <kevin.lin.p@gmail.com>
2016-05-25 09:07:21 -0700

    Adding static website to brunch

--- brunch-config.js
+++ brunch-config.js
@@ -24,7 +24,9 @@ exports.config = {
       joinTo: "css/app.css"
     },
     templates: {
-      joinTo: "js/app.js"
+      joinTo: {
+        'js/app.js': /.+\.jade$/
+      }
     }
   },
 
@@ -52,6 +54,9 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
+    },
+    static_jade: {
+      asset: "priv/"
     }
   },
 
@@ -62,9 +67,12 @@ exports.config = {
   },
 
   npm: {
-    enabled: true,
     // Whitelist the npm deps to be pulled in as front-end assets.
     // All other deps in package.json will be excluded from the bundle.
-    whitelist: ["phoenix", "phoenix_html"]
+    globals: {
+      //phoenix: 'phoenix',
+      //phoenix_html: 'phoenix_html'
+    }
   }
 };
+
---
+++ web/static/js/google-analytics.js
@@ -0,0 +1,16 @@
+// Brunch automatically concatenates all files in your
+// watched paths. Those paths can be configured at
+// config.paths.watched in "brunch-config.js".
+//
+// However, those files will only be executed if
+// explicitly imported. The only exception are files
+// in vendor, which are never wrapped in imports and
+// therefore are always executed.
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-62173532-1', 'auto');
+ga('send', 'pageview');
